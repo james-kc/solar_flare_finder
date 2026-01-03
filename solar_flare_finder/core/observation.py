0af0d22ed252c32da33d04a3fca46580de59a059
JamesK-C <j.kavanaghcranston@gmail.com>
2026-01-03 23:51:09 +0000

    Ngl, this is cursed. I wanna link the prev commit to the issue so I'm just deleting it and then re-committing it...

--- solar_flare_finder/core/observation.py
+++ solar_flare_finder/core/observation.py
@@ -15,13 +15,3 @@ class InstrumentObservation:
     """Fraction of the flare rise phase observed."""
     frac_obs_fall: float | None
     """Fraction of the flare fall phase observed."""
-
-    def __post_init__(self):
-        if self.observed and all(
-            x is None for x in [self.frac_obs, self.frac_obs_rise, self.frac_obs_fall]
-        ):
-            raise ValueError("observed=True but all fraction values are None")
-        elif not self.observed and any(
-            x is not None for x in [self.frac_obs, self.frac_obs_rise, self.frac_obs_fall]
-        ):
-            raise ValueError("observed=False but fraction values are provided")
