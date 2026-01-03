0950179ff08efb0919af9f0e4deac6e62dc272f6
JamesK-C <j.kavanaghcranston@gmail.com>
2026-01-03 23:26:11 +0000

    Added initial SolarFlare definition (Closes #1)

---
+++ solar_flare_finder/core/__init__.py
@@ -0,0 +1,7 @@
+from .flare import SolarFlare
+from .observation import InstrumentObservation
+
+__all__ = [
+    "SolarFlare",
+    "InstrumentObservation",
+]
--- solar_flare_finder/core/flare.py
+++ solar_flare_finder/core/flare.py
@@ -1,12 +1,20 @@
-# core/flare.py
 from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
-class Flare:
+class SolarFlare:
+    """Represents a solar flare event with timing and location data."""
     start: datetime
+    """Flare start time (UTC)."""
     peak: datetime
+    """Flare peak intensity time (UTC)."""
     end: datetime
+    """Flare end time (UTC)."""
     goes_class: str
-    location: str
-    noaa_ar: int | None
+    """GOES X-ray classification (A, B, C, M, or X)."""
+    loc_hgs: str
+    """Stonyhurst Heliographic Coordinates (HGS) location."""
+    loc_x: float
+    """Helioprojective Cartesian X coordinate (arcsec)."""
+    loc_y: float
+    """Helioprojective Cartesian Y coordinate (arcsec)."""
